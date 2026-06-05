30b65d943360cee215ef5f781cc6661e7948b39e
avinash <avinash.paymode07@gmail.com>
2026-06-05 10:06:24 +0530

    Added basic sequelize & cli related files

---
+++ src/routes/v2/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+
+const router = express.Router();
+
+router.get('/info', (req, res) => {
+    return  res.json({msg: "comming from v2 api"});
+});
+
+module.exports = router;
